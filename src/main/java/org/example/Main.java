d94485e05079122b85348ebd783ec9f75b7c201b
piyush <piyushagrawal67944@gmail.com>
2025-02-10 21:40:42 -0500

    updating code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int[] arr = new int[5];
+        int arr1 [] = new int[5];
+        List<Integer> list = new LinkedList<>();
+
+        System.out.println(arr.length);
+        System.out.println(list.size());
+
+
     }
 }
--- src/main/java/org/example/week3/ExerciseAndDownloads/Cube.java
+++ src/main/java/org/example/week3/ExerciseAndDownloads/Cube.java
@@ -1,38 +1,35 @@
 package org.example.week3.ExerciseAndDownloads;// A class that models a cube
 
-class Cube
-{
- private int side;  // value for the side of a cube
+class Cube {
+    private int side;  // value for the side of a cube
 
- public Cube(int side) {
-  this.side = side;
- }
+    public Cube(int side) {
+        this.side = side;
+    }
 
- public Cube() {
- }
- public void setSide( int sideValue )
- {
-  editSide( sideValue );
- }
+    public Cube() {
+    }
 
- public int getVolume()
- {
-  return side * side * side;
- }
- 
- private void editSide( int value )
- {
-  if ( value > 0 )
-    side = value;
-  else
-    System.out.println( "\nERROR: Side values must be > 0" );
- }
+    public void setSide(int sideValue) {
+        editSide(sideValue);
+    }
 
- public int getSide() {
-  return side;
- }
+    public int getVolume() {
+        return side * side * side;
+    }
 
- public int getSurfaceArea() {
-  return 6*side*side;
- }
+    private void editSide(int value) {
+        if (value > 0)
+            side = value;
+        else
+            System.out.println("\nERROR: Side values must be > 0");
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public int getSurfaceArea() {
+        return 6 * side * side;
+    }
 }
